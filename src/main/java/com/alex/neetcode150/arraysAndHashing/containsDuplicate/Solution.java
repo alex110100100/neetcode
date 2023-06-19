6da38abc9e05f9caf2a04199bafa070559e91653
Alex <97766522+alex110100100@users.noreply.github.com>
2023-06-19 19:11:56 +0100

    Move Java classes into one file

--- src/main/java/com/alex/neetcode150/arraysAndHashing/containsDuplicate/Solution.java
+++ src/main/java/com/alex/neetcode150/arraysAndHashing/containsDuplicate/Solution.java
@@ -14,3 +14,20 @@ class Solution {
         return false;
     }
 }
+
+
+class Test {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] nums1 = {1, 2, 3, 4, 5};      // No duplicates
+        int[] nums2 = {1, 2, 3, 1};         // Contains duplicate (1)
+        int[] nums3 = {5, 5, 6, 7, 8};      // Contains duplicate (5)
+        int[] nums4 = {1, 1, 1, 1, 1};      // Contains duplicate (1)
+
+        System.out.println(solution.containsDuplicate(nums1));   // Expected output: false
+        System.out.println(solution.containsDuplicate(nums2));   // Expected output: true
+        System.out.println(solution.containsDuplicate(nums3));   // Expected output: true
+        System.out.println(solution.containsDuplicate(nums4));   // Expected output: true
+    }
+}
